085c0554f6583046848c6979b2b415676afffdcb
r00ster91 <r00ster91@protonmail.com>
2021-08-21 09:15:59 +0200

    use const for probability

--- src/grid/random.rs
+++ src/grid/random.rs
@@ -5,10 +5,14 @@ use rand::SeedableRng;
 use super::{Cell, Grid};
 use terminal::util::Size;
 
+const PROBABILITY: f64 = 0.75;
+
 fn random_cells(size: u32) -> Vec<Cell> {
     let mut cells = Vec::<Cell>::with_capacity(size as usize);
     let mut small_rng = SmallRng::from_entropy();
-    let distribution = Bernoulli::new(0.75).unwrap();
+
+    let distribution = Bernoulli::new(PROBABILITY);
+    let distribution = distribution.unwrap(); // `PROBABILITY` is in range 0 to 1
 
     for _ in 0..size {
         let filled = distribution.sample(&mut small_rng);
