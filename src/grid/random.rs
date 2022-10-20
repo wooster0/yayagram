1d6c9620018164c07603fedc8677551d3964860b
r00ster91 <r00ster91@proton.me>
2022-10-20 18:13:24 +0200

    deps: replace rand with fastrand
    The fastrand crate is smaller, and probably faster than the rand crate:
    https://github.com/bevyengine/bevy/pull/3992#pullrequestreview-889064314

--- src/grid/random.rs
+++ src/grid/random.rs
@@ -1,23 +1,11 @@
-use rand::distributions::{Bernoulli, Distribution};
-use rand::rngs::SmallRng;
-use rand::SeedableRng;
-
 use super::{Cell, Grid};
 use terminal::util::Size;
 
-const PROBABILITY: f64 = 0.75;
-
 fn random_cells(size: u32) -> Vec<Cell> {
     let mut cells = Vec::<Cell>::with_capacity(size as usize);
-    let mut small_rng = SmallRng::from_entropy();
-
-    let distribution = Bernoulli::new(PROBABILITY);
-    let distribution = distribution.unwrap(); // `PROBABILITY` is in range 0 to 1
 
     for _ in 0..size {
-        let filled = distribution.sample(&mut small_rng);
-
-        cells.push(Cell::from(filled));
+        cells.push(Cell::from(fastrand::bool()));
     }
 
     cells
